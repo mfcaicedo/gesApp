8cbd1af06aea1a0820e4164e39a402a319a5fc24
mfcaicedo <mfcaicedo@unicauca.edu.co>
2024-02-20 00:01:12 -0500

    feat: Se configura NativeBase ultima version gluestack

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,15 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { GluestackUIProvider, Text, Box, Center } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
 
 const IndexApp = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hola mundo, GesApp</Text>
-      <View style={styles.separator} />
-    </View>
+    <GluestackUIProvider config={config} >
+      <Center  bg='red' h={600} w={300} marginVertical={'auto'} marginHorizontal={0} >
+        {/* <Box width="100%" justifyContent="center" alignItems="center"> */}
+          <Text>GesApp con nativeBase</Text>
+        {/* </Box> */}
+      </Center>
+    </GluestackUIProvider>
   );
 }
-
 export default IndexApp;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
