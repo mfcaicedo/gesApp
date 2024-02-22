e9d1726665e0b7c55ad661fccf77ad30fb3a5c83
mfcaicedo <mfcaicedo@unicauca.edu.co>
2024-02-22 00:41:30 -0500

    feat: Add view Welcome to app

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,23 +1,31 @@
 import {
-    GluestackUIProvider, Text, Box, Center, Button, ButtonText, ButtonIcon, AddIcon
+    GluestackUIProvider, Text, Box, Center, Button, ButtonText, Image, Heading
 } from '@gluestack-ui/themed';
-import { config } from "../../../config/gluestack-ui.config"
+import { config } from "../../../config/gluestack-ui.config";
+const logoGesApp = require('../../../assets/images/logo-grande.jpg');
 
 const Index = () => {
     return (
         <GluestackUIProvider config={config} >
-
-            <Center>
-                <Text bold >Bienvenido a GesApp</Text>
-                <Box>
-                    <Text>
-                        Ahora desde aquí prodrá gestionar su comunidad!
-                    </Text>
-                    <Button size="md" variant="solid" bgColor='$primary500' action="primary" isDisabled={false} isFocusVisible={false} >
-                        <ButtonText>Continuar</ButtonText>
-                    </Button>
-                </Box>
-            </Center>
+            <Box justifyContent="center" h="100%">
+                <Center>
+                    <Image
+                        size="2xl" borderRadius="$sm"
+                        source={logoGesApp}
+                        alt="GesApp"
+                        mb="$12"
+                    />
+                    <Heading bold size="2xl" >Bienvenido a GesApp</Heading>
+                    <Box>
+                        <Text bold size="xl">
+                            Ahora desde aquí prodrá gestionar su comunidad!
+                        </Text>
+                        <Button size="md" mt="$20" variant="solid" bgColor='$primary500' action="primary" isDisabled={false} isFocusVisible={false} >
+                            <ButtonText>Continuar</ButtonText>
+                        </Button>
+                    </Box>
+                </Center>
+            </Box>
         </GluestackUIProvider>
     )
 }
