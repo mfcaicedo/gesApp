82431d867fe73e66fb517c0169270cf2cbf30574
mfcaicedo <mfcaicedo@unicauca.edu.co>
2024-08-05 00:36:27 -0500

    feat: se configura firebase para la app gesApp

--- src/utils/constants/firebase.ts
+++ src/utils/constants/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID} from '@env';
+import {API_KEY, AUTH_DOMAIN, PROJECT_ID, DATABASE_URL, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID} from '@env';
 
 const firebaseConfig = {
-  apiKey: API_KEY,
+  apiKey: API_KEY,  
   authDomain: AUTH_DOMAIN,
   projectId: PROJECT_ID,
+  databaseURL: DATABASE_URL,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
@@ -17,5 +18,5 @@ const firebaseConfig = {
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
-export const FIREBASE_DB = getFirestore();
+}); 
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
